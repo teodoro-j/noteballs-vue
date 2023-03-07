4d26eb711aee201540f4f4f3fecd913e7edd5c00
teodorojo <joaoalfredoteodoro@gmail.com>
2023-03-07 17:01:25 -0300

    feature-add-routes

--- api-noteballs-vue/src/main.js
+++ api-noteballs-vue/src/main.js
@@ -1,6 +1,24 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import App from './App.vue';
+import { createApp } from 'vue';
+import ViewNotes from '@/views/ViewNotes.vue';
+import ViewStats from '@/views/ViewStats.vue';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
-import './assets/main.css'
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: [
+        {
+            path: '/',
+            name: 'notes',
+            component: ViewNotes
+        },
+        {
+            path: '/stats',
+            name: 'stats',
+            component: ViewStats
+        }
+    ]
+})
+
+createApp(App).use(router).mount('#app')

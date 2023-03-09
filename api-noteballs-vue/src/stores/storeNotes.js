78101d2699b584452c0e6bd6cc0cb21b137322e0
teodorojo <joaoalfredoteodoro@gmail.com>
2023-03-09 14:18:50 -0300

    feature(firebase) add note conextion with googles firebase

--- api-noteballs-vue/src/stores/storeNotes.js
+++ api-noteballs-vue/src/stores/storeNotes.js
@@ -1,32 +1,45 @@
-import { defineStore } from 'pinia'
+import { defineStore } from 'pinia';
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { db } from '../js/firebase';
 
 export const useStoreNotes = defineStore('storeNotes', {
-  state: () => {
-    return { 
-        notes: [
-            {
-                id: 'id1',
-                content: 'This is the first note!'
-            }
-        ] 
-    }
-  },
-  actions: {
-    addNote(newNoteContent) {
-        let atualDate= new Date().getTime( );
-        let id = atualDate.toString();
-    
-        let note = {
-            id,
-            content: newNoteContent
+    state: () => {
+        return {
+            notes: [
+                // {
+                //     id: 'id1',
+                //     content: 'This is the first note!'
+                // }
+            ]
         }
-        
-        this.notes.unshift(note);
     },
-    deleteNote(idToDelete) {
-        this.notes = this.notes.filter(note => {
-            return note.id !== idToDelete
-        })
-    }
-  },
+    actions: {
+        async getNotes() {
+            const querySnapshot = await getDocs(collection(db, "notes"));
+            querySnapshot.forEach((doc) => {
+                console.log(doc.id, " => ", doc.data());
+                let note = {
+                    id: doc.id,
+                    content: doc.data().content,
+                }
+                this.notes.push(note)
+            });
+        },
+        addNote(newNoteContent) {
+            let atualDate = new Date().getTime();
+            let id = atualDate.toString();
+
+            let note = {
+                id,
+                content: newNoteContent
+            }
+
+            this.notes.unshift(note);
+        },
+        deleteNote(idToDelete) {
+            this.notes = this.notes.filter(note => {
+                return note.id !== idToDelete
+            })
+        }
+    },
 })
